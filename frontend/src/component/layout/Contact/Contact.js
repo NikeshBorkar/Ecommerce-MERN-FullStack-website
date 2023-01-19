4661d1ff5cd34649676d91cdc7f0b341e70f5118
Nikesh Borkar <99666800+NikeshBorkar@users.noreply.github.com>
2023-01-19 17:14:33 +0530

    title aaded

--- frontend/src/component/layout/Contact/Contact.js
+++ frontend/src/component/layout/Contact/Contact.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./Contact.css";
 import { Button } from "@material-ui/core";
+import MetaData from "../MetaData";
 
 const Contact = () => {
   return (
     <div className="contactContainer">
+    <MetaData title="CONTACT US" />
       <a className="mailBtn" href="mailto:Nikeshborkar008@gmail.com">
         <Button>Contact: Nikeshborkar008@gmail.com</Button>
       </a>
